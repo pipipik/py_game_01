41f61bd4d977b55200e98b6f4e3794d2195f3649
pipipik <hiro.konan913@gmail.com>
2023-11-30 23:29:56 +0900

    draw_rect

--- draw_01.py
+++ draw_01.py
@@ -1,7 +1,7 @@
 """draw_01.py"""
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, Rect
 
 pygame.init() # pygameモジュールを初期化
 SURFACE = pygame.display.set_mode((400, 300)) #サイズを指定してウィンドウを作成
@@ -19,6 +19,20 @@ def main():
 
     SURFACE.fill((255, 255, 255)) # ウィンドウを白色(R,G,B)に塗りつぶす
     
+    # rect(SURFACE: ベース画面オブジェクト, color: 色, (x, y, width, height): 位置, サイズ, width: 線の幅)
+    # 赤 : 矩形(塗りつぶし)
+    pygame.draw.rect(SURFACE, (255,0,0), (10, 20, 100, 50))
+    # 赤 : 矩形(太さ 3)
+    pygame.draw.rect(SURFACE, (255, 0, 0), (150, 10, 100, 30), 3)
+    # 緑 : 矩形
+    pygame.draw.rect(SURFACE, (0,255,0), ((100, 80), (80, 50)))
+    # 青 : 矩形、Rectオブジェクト
+    rect0 = Rect(200, 60, 140, 80)
+    pygame.draw.rect(SURFACE, (0, 0, 255), rect0)
+    # 黄 : 矩形、Rectオブジェクト
+    rect1 = Rect((30, 160), (100, 50))
+    pygame.draw.rect(SURFACE, (255, 255, 0), rect1)
+
     pygame.display.update() # プログラム中に描画した内容を画面に反映
     FPSCLOCK.tick(10) # 1秒間に10回ループが実行
 
