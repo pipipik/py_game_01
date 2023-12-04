d3ecdfa6fa6156fad2103bcbc08d69f842c3b9c6
pipipik <hiro.konan913@gmail.com>
2023-12-04 23:33:54 +0900

    draw_polygon

--- draw_02.py
+++ draw_02.py
@@ -1,8 +1,8 @@
 """draw_polygon.py"""
 import sys
-import random
+from math import sin, cos, radians
 import pygame
-from pygame.locals import QUIT, Rect
+from pygame.locals import QUIT
 
 pygame.init() # pygameモジュールを初期化
 SURFACE = pygame.display.set_mode((400, 300)) #サイズを指定してウィンドウを作成
@@ -20,6 +20,17 @@ def main():
 
     SURFACE.fill((0, 0, 0)) # ウィンドウを黒色(R,G,B)に塗りつぶす
     
+    # 多角形(ポリゴン)
+    pointlist0, pointlist1 = [], [] # 点のリストを初期化
+    for theta in range(0, 360, 72): # 0°から360°まで72ずつの幅で増加するtheta
+      rad = radians(theta) #度数法[°]→弧度法[rad]に単位変換
+      pointlist0.append((cos(rad)*100 + 100, sin(rad)*100 + 150)) # (100, 150)を中心に点のリストを作成
+      pointlist1.append((cos(rad)*100 + 300, sin(rad)*100 + 150)) # (300, 150)を中心に点のリストを作成
+    # linesで作成した多角形
+    pygame.draw.lines(SURFACE, (255, 255, 255), True, pointlist0)
+    # lines(SURFACE: ベース画面オブジェクト, color: 色, pointlist: 点のリスト, width: 線の幅(0の時は塗りつぶし))
+    pygame.draw.polygon(SURFACE, (255, 255, 255), pointlist1)
+
     pygame.display.update() # プログラム中に描画した内容を画面に反映
     FPSCLOCK.tick(1) # 1秒間に10回ループが実行
 
