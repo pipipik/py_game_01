c24a9c0e0074de27cdad3f1277199b83cf5a0785
pipipik <hiro.konan913@gmail.com>
2023-12-06 21:05:54 +0900

    draw_text1

--- draw_04.py
+++ draw_04.py
@@ -9,6 +9,13 @@ FPSCLOCK = pygame.time.Clock() # クロックオブジェクトを作成
 
 def main():
   """main routine"""
+  # pygame.font.SysFont(name: フォント名(デフォルトはNone), size: フォントサイズ,
+  #                     bold: 太字か否か(省略時はFalse), italic: イタリックか否か(省略時はFalse))
+  sysfont = pygame.font.SysFont(None, 72) # Fontオブジェクト作成
+  # render(text: 描画するテキスト, antialias: アンチエイリアス(輪郭をスムーズに), color: 色, background: 背景色(省略時はFalse))
+  message = sysfont.render("Hello Python", True, (0, 128, 128)) # ビットマップ(Surface)を作成
+  message_rect = message.get_rect() # 画像の占める矩形をget_rect()メソッドで取得
+  message_rect.center = (200, 200) # プロパティに中心の座標を設定
 
   while True:
     for event in pygame.event.get(): # イベントキューからイベントを取得
@@ -18,6 +25,8 @@ def main():
         sys.exit() # プログラム終了
 
     SURFACE.fill((255, 255, 255)) # ウィンドウを白色(R,G,B)に塗りつぶす
+    # コピー先のSURFACEオブジェクト.blit(source: コピー元のSurfaceオブジェクト, dest: コピーする座標(左上))
+    SURFACE.blit(message, message_rect)
 
     pygame.display.update() # プログラム中に描画した内容を画面に反映
     FPSCLOCK.tick(1) # 1秒間に1回ループが実行
