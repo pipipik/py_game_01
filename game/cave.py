c58c04f14237f7c7613c52002a5fbd3d9b5246fa
pipipik <hiro.konan913@gmail.com>
2023-12-07 22:37:43 +0900

    just_window

---
+++ game/cave.py
@@ -0,0 +1,27 @@
+"""cave - Copyright 20231201_01"""
+import sys
+import pygame
+from pygame.locals import QUIT
+
+pygame.init() # pygameモジュールを初期化
+SURFACE = pygame.display.set_mode((800, 600)) #サイズを指定してウィンドウを作成
+FPSCLOCK = pygame.time.Clock() # クロックオブジェクトを作成
+
+def main():
+  """main routine"""
+
+  while True:
+    for event in pygame.event.get(): # イベントキューからイベントを取得
+      # 終了イベントを検出した時にプログラムを終了する
+      if event.type == QUIT: # イベントがQUITの時
+        pygame.quit() # pygameの初期化を解除
+        sys.exit() # プログラム終了
+
+    SURFACE.fill((0, 255, 0)) # ウィンドウを緑色(R,G,B)に塗りつぶす
+
+    pygame.display.update() # プログラム中に描画した内容を画面に反映
+    FPSCLOCK.tick(10) # 1秒間に10回ループが実行
+
+# 自ファイルから開始された時にmain関数が実行
+if __name__ == '__main__':
+  main()
