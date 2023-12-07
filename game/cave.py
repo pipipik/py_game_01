affddb5144b843099f2fdcf6d2275b6a0e0ad055
pipipik <hiro.konan913@gmail.com>
2023-12-07 23:00:49 +0900

    draw_rect

--- game/cave.py
+++ game/cave.py
@@ -1,7 +1,7 @@
 """cave - Copyright 20231201_01"""
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, Rect
 
 pygame.init() # pygameモジュールを初期化
 SURFACE = pygame.display.set_mode((800, 600)) #サイズを指定してウィンドウを作成
@@ -9,6 +9,10 @@ FPSCLOCK = pygame.time.Clock() # クロックオブジェクトを作成
 
 def main():
   """main routine"""
+  walls = 80 # 矩形の数
+  holes = [] # 矩形を格納する配列を初期化
+  for xpos in range(walls): # x座標を10ずつずらしながら80個の矩形を作成
+    holes.append(Rect(xpos * 10, 100, 10, 400)) # (x, y, width, height)
 
   while True:
     for event in pygame.event.get(): # イベントキューからイベントを取得
@@ -17,7 +21,10 @@ def main():
         pygame.quit() # pygameの初期化を解除
         sys.exit() # プログラム終了
 
+    # 描画
     SURFACE.fill((0, 255, 0)) # ウィンドウを緑色(R,G,B)に塗りつぶす
+    for hole in holes: # 画面オブジェクトに黒色の矩形リストholesを描画
+      pygame.draw.rect(SURFACE, (0, 0, 0), hole)
 
     pygame.display.update() # プログラム中に描画した内容を画面に反映
     FPSCLOCK.tick(10) # 1秒間に10回ループが実行
