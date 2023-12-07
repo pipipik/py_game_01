1078b46e4a208bdcbfe8b6658929b6836142f270
pipipik <hiro.konan913@gmail.com>
2023-12-07 23:34:43 +0900

    scroll

--- game/cave.py
+++ game/cave.py
@@ -1,5 +1,6 @@
 """cave - Copyright 20231201_01"""
 import sys
+from random import randint
 import pygame
 from pygame.locals import QUIT, Rect
 
@@ -11,6 +12,7 @@ def main():
   """main routine"""
   walls = 80 # 矩形の数
   holes = [] # 矩形を格納する配列を初期化
+  slope = randint(1, 6) # 傾きを指定(randint: 0～6の範囲の整数値をランダムに取得)
   for xpos in range(walls): # x座標を10ずつずらしながら80個の矩形を作成
     holes.append(Rect(xpos * 10, 100, 10, 400)) # (x, y, width, height)
 
@@ -20,6 +22,17 @@ def main():
       if event.type == QUIT: # イベントがQUITの時
         pygame.quit() # pygameの初期化を解除
         sys.exit() # プログラム終了
+
+    # 洞窟をスクロール
+    edge = holes[-1].copy() # 右端の矩形ををコピーしてedgeに格納
+    test = edge.move(0, slope) # .move(x, y)->Rect y軸方向にslope移動
+    if test.top <= 0 or test.bottom >= 600: # test(Rectオブジェクト)が天井、床に衝突したら
+      slope = randint(1, 6) * (-1 if slope > 0 else 1) # 傾きを変更
+      edge.inflate_ip(0, -20) # .inflate(x, y)->Rect y軸方向のサイズを-20縮小
+    edge.move_ip(10, slope) # .move_ip(x, y) x軸方向に10, y軸方向にslope移動
+    holes.append(edge) # 右端に追加
+    del holes[0] # 左端を削除
+    holes = [x.move(-10, 0) for x in holes] # .move(x, y) 全体を左へ10移動
 
     # 描画
     SURFACE.fill((0, 255, 0)) # ウィンドウを緑色(R,G,B)に塗りつぶす
