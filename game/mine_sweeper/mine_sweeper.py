e1cf850e5c373a53dcb4009ea8b9f5e1bb30ef94
pipipik <hiro.konan913@gmail.com>
2023-12-09 08:46:00 +0900

    set_window

--- game/mine_sweeper/mine_sweeper.py
+++ game/mine_sweeper/mine_sweeper.py
@@ -3,8 +3,13 @@ import sys
 import pygame
 from pygame.locals import QUIT
 
+# グローバル変数定義
+WIDTH = 20 # 横方向のマスの数
+HEIGHT = 15 # 縦方向のマスの数
+SIZE = 50 # 1マスの縦横のサイズ
+
 pygame.init() # pygameモジュールを初期化
-SURFACE = pygame.display.set_mode((800, 600)) #サイズを指定してウィンドウを作成
+SURFACE = pygame.display.set_mode([WIDTH*SIZE, HEIGHT*SIZE]) #サイズを指定してウィンドウを作成
 FPSCLOCK = pygame.time.Clock() # クロックオブジェクトを作成
 
 def main():
@@ -17,7 +22,17 @@ def main():
         pygame.quit() # pygameの初期化を解除
         sys.exit() # プログラム終了
 
+    # 描画
     SURFACE.fill((0, 0, 0)) # ウィンドウを黒色(R,G,B)に塗りつぶす
+
+    # 線の描画
+    # line(SURFACE: ベース画面オブジェクト, color: 色, start_pos: 始点, end_pos: 終点, width: 線の幅)
+    # 縦線
+    for index in range(0, WIDTH*SIZE, SIZE): # 左端から右端までタイルのサイズごとにx座標を指定
+      pygame.draw.line(SURFACE, (96, 96, 96), (index, 0), (index, HEIGHT*SIZE))
+    # 横線
+    for index in range(0, HEIGHT*SIZE, SIZE): # 上端から下端までタイルのサイズごとにy座標を指定
+      pygame.draw.line(SURFACE, (96, 96, 96), (0, index), (WIDTH*SIZE, index))
 
     pygame.display.update() # プログラム中に描画した内容を画面に反映
     FPSCLOCK.tick(1) # 1秒間に1回ループが実行
