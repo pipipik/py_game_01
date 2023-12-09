85a0079abad7603b27a594ec519120a6637b798a
pipipik <hiro.konan913@gmail.com>
2023-12-09 09:44:47 +0900

    set_window_02

--- game/mine_sweeper/mine_sweeper.py
+++ game/mine_sweeper/mine_sweeper.py
@@ -7,6 +7,7 @@ from pygame.locals import QUIT
 WIDTH = 20 # 横方向のマスの数
 HEIGHT = 15 # 縦方向のマスの数
 SIZE = 50 # 1マスの縦横のサイズ
+EMPTY = 0 # マップ上のタイルがからの状態
 
 pygame.init() # pygameモジュールを初期化
 SURFACE = pygame.display.set_mode([WIDTH*SIZE, HEIGHT*SIZE]) #サイズを指定してウィンドウを作成
@@ -14,6 +15,9 @@ FPSCLOCK = pygame.time.Clock() # クロックオブジェクトを作成
 
 def main():
   """main routine"""
+
+  # field1状態をからで初期化 field(list) = [0, 0, ...], [0, ...], ...
+  field = [[EMPTY for xpos in range(WIDTH)] for ypos in range(HEIGHT)]
 
   while True:
     for event in pygame.event.get(): # イベントキューからイベントを取得
@@ -24,6 +28,19 @@ def main():
 
     # 描画
     SURFACE.fill((0, 0, 0)) # ウィンドウを黒色(R,G,B)に塗りつぶす
+
+
+    """ field[index]
+    [0,0][0,1][0,2][0,3]
+    [1,0][1,1][1,2][1,3]
+    [2,0][2,1][2,3][2,3]
+    """
+    for ypos in range(HEIGHT):
+      for xpos in range(WIDTH):
+        tile = field[ypos][xpos] # 左上から順にfield状態をtileに格納
+        rect = (xpos*SIZE, ypos*SIZE, SIZE, SIZE) # tileの矩形サイズを取得
+        if tile == EMPTY: # field状態がENPTYの時
+          pygame.draw.rect(SURFACE, (192, 192, 192), rect) # 未開封状態を描画
 
     # 線の描画
     # line(SURFACE: ベース画面オブジェクト, color: 色, start_pos: 始点, end_pos: 終点, width: 線の幅)
