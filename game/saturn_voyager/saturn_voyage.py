aaf047fba9b824caf1a58f4a36c285005d729771
pipipik <hiro.konan913@gmail.com>
2023-12-11 22:32:08 +0900

    set_scope

--- game/saturn_voyager/saturn_voyage.py
+++ game/saturn_voyager/saturn_voyage.py
@@ -14,6 +14,7 @@ def main():
   ship = [0, 0] # 自機の座標
   speed = 25 # スピード(時間経過と共に加速)
   rock_image = pygame.image.load("rock.png") # 隕石_image
+  scope_image = pygame.image.load("scope.png") # 自機_image
 
   while len(stars) < 200: # 隕石を200個(x, y, z)座標と回転角をランダムに配置
     stars.append({
@@ -31,6 +32,7 @@ def main():
     for star in stars:
       star["pos"][2] -= speed # z座標をスピード分減らす(近づける)
 
+    # 描画
     SURFACE.fill((0, 0, 0)) # ウィンドウを黒色(R,G,B)に塗りつぶす
     stars = sorted(stars, key=lambda x: x["pos"][2], reverse=True) # x["pos"][2](z軸)の大きい順にソート
     for star in stars:
@@ -44,6 +46,8 @@ def main():
       # rotozoom(Surface: 回転とズームを行うSurface, angle: 回転角, scale: ズームの倍率)
       rotated = pygame.transform.rotozoom(rock_image, star["theta"], size / 145)
       SURFACE.blit(rotated, (xpos, ypos))
+
+    SURFACE.blit(scope_image, (0, 0)) # 自機の描画
 
     pygame.display.update() # プログラム中に描画した内容を画面に反映
     FPSCLOCK.tick(15) # 1秒間に15回ループが実行
