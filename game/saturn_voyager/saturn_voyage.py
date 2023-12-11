33a6208ff14ca2c49a72cca21230f840907c898a
pipipik <hiro.konan913@gmail.com>
2023-12-11 22:50:10 +0900

    control_scope

--- game/saturn_voyager/saturn_voyage.py
+++ game/saturn_voyager/saturn_voyage.py
@@ -2,7 +2,7 @@
 import sys
 from random import randint
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, KEYDOWN, KEYUP, K_LEFT, K_RIGHT, K_UP, K_DOWN
 
 pygame.init() # pygameモジュールを初期化
 SURFACE = pygame.display.set_mode((800, 800)) #サイズを指定してウィンドウを作成
@@ -13,6 +13,7 @@ def main():
   stars = [] # 隕石を格納するリスト
   ship = [0, 0] # 自機の座標
   speed = 25 # スピード(時間経過と共に加速)
+  keymap = [] # どのキーが押下されているかを示すリスト
   rock_image = pygame.image.load("rock.png") # 隕石_image
   scope_image = pygame.image.load("scope.png") # 自機_image
 
@@ -27,6 +28,26 @@ def main():
       if event.type == QUIT: # イベントがQUITの時
         pygame.quit() # pygameの初期化を解除
         sys.exit() # プログラム終了
+      elif event.type == KEYDOWN: # キー押下
+        if not event.key in keymap: # keymapになければイベントを追加
+          keymap.append(event.key)
+      elif event.type == KEYUP: # キーを離せばイベントを削除
+        keymap.remove(event.key)
+
+    # フレーム毎の処理
+    if K_LEFT in keymap: # 左キー
+      ship[0] -= 30
+    elif K_RIGHT in keymap: # 右キー
+      ship[0] += 30
+    elif K_UP in keymap: # 上キー
+      ship[1] -= 30
+    elif K_DOWN in keymap: # 下キー
+      ship[1] += 30
+
+    # min(800, ship[0]): 800以下(上限ガード)
+    # max(-800, ship[0]): -800以上(下限ガード)
+    ship[0] = max(-800, min(800, ship[0])) # x座標のガード
+    ship[1] = max(-800, min(800, ship[1])) # y座標のガード
 
     # 隕石の移動
     for star in stars:
