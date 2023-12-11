208828f5b03079c8594ac4bf9bcf77ab048ca21a
pipipik <hiro.konan913@gmail.com>
2023-12-11 22:26:28 +0900

    move_rock

--- game/saturn_voyager/saturn_voyage.py
+++ game/saturn_voyager/saturn_voyage.py
@@ -12,6 +12,7 @@ def main():
   """main routine"""
   stars = [] # 隕石を格納するリスト
   ship = [0, 0] # 自機の座標
+  speed = 25 # スピード(時間経過と共に加速)
   rock_image = pygame.image.load("rock.png") # 隕石_image
 
   while len(stars) < 200: # 隕石を200個(x, y, z)座標と回転角をランダムに配置
@@ -25,6 +26,10 @@ def main():
       if event.type == QUIT: # イベントがQUITの時
         pygame.quit() # pygameの初期化を解除
         sys.exit() # プログラム終了
+
+    # 隕石の移動
+    for star in stars:
+      star["pos"][2] -= speed # z座標をスピード分減らす(近づける)
 
     SURFACE.fill((0, 0, 0)) # ウィンドウを黒色(R,G,B)に塗りつぶす
     stars = sorted(stars, key=lambda x: x["pos"][2], reverse=True) # x["pos"][2](z軸)の大きい順にソート
@@ -41,7 +46,7 @@ def main():
       SURFACE.blit(rotated, (xpos, ypos))
 
     pygame.display.update() # プログラム中に描画した内容を画面に反映
-    FPSCLOCK.tick(1) # 1秒間に1回ループが実行
+    FPSCLOCK.tick(15) # 1秒間に15回ループが実行
 
 # 自ファイルから開始された時にmain関数が実行
 if __name__ == '__main__':
