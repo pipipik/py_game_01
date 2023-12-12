59909ce7c4377ff6c52377ec8d3120366a808451
pipipik <hiro.konan913@gmail.com>
2023-12-12 21:13:21 +0900

    move_snake

--- game/snake/snake_bite.py
+++ game/snake/snake_bite.py
@@ -2,20 +2,21 @@
 import sys
 import random
 import pygame
-from pygame.locals import QUIT, Rect
+from pygame.locals import QUIT, Rect, KEYDOWN, K_LEFT, K_RIGHT, K_UP, K_DOWN
 
 pygame.init() # pygameモジュールを初期化
 SURFACE = pygame.display.set_mode((600, 600)) #サイズを指定してウィンドウを作成
 FPSCLOCK = pygame.time.Clock() # クロックオブジェクトを作成
 
 FOODS = [] # 餌の座標を格納した配列
+SNAKE = [] # へびの座標を格納した配列
 (W, H) = (20, 20) # 画面の幅Wと高さH
 
 def add_food():
   """ ランダムな場所に餌を配置 """
   while True:
     pos = (random.randint(0, W-1), random.randint(0, H-1)) # ランダムな座標を指定
-    if pos in FOODS: # 重複を回避
+    if pos in FOODS or pos in SNAKE: # 重複を回避
       continue # ループの先頭に戻る
     FOODS.append(pos) # 座標を追加
     break # ループから抜けて呼び出し元に戻る
@@ -26,6 +27,9 @@ def paint():
   for food in FOODS: # 配列から座標を取り出し、円を描画
     pygame.draw.ellipse(SURFACE, (0, 255, 0), Rect(food[0]*30, food[1]*30, 30, 30))
 
+  for body in SNAKE: # 配列から座標を取り出し、矩形を描画
+    pygame.draw.rect(SURFACE, (0, 255, 255), Rect(body[0]*30, body[1]*30, 30, 30))
+
   for index in range(20): # 20*20マス作成
     pygame.draw.line(SURFACE, (64, 64, 64), (index*30, 0), (index*30, 600))
     pygame.draw.line(SURFACE, (64, 64, 64), (0, index*30), (600, index*30))
@@ -34,6 +38,8 @@ def paint():
 
 def main():
   """main routine"""
+  key = K_DOWN # 初期状態は下キー
+  SNAKE.append((int(W/2), int(H/2))) # 初期状態は画面中央
   for _ in range(10):
     add_food() # 餌を10個配置
 
@@ -43,6 +49,20 @@ def main():
       if event.type == QUIT: # イベントがQUITの時
         pygame.quit() # pygameの初期化を解除
         sys.exit() # プログラム終了
+      elif event.type == KEYDOWN: # キー押下
+        key = event.key
+
+    if key == K_LEFT: # 左キー: x座標を-1
+      head = (SNAKE[0][0] - 1, SNAKE[0][1])
+    elif key == K_RIGHT: # 右キー: x座標を+1
+      head = (SNAKE[0][0] + 1, SNAKE[0][1])
+    elif key == K_UP: # 上キー: y座標を-1
+      head = (SNAKE[0][0], SNAKE[0][1] - 1)
+    elif key == K_DOWN: # 下キー: y座標を+1
+      head = (SNAKE[0][0], SNAKE[0][1] + 1)
+
+
+    SNAKE.insert(0, head) # キーに応じて要素を追加
 
     paint() # 描画関数呼び出し
     FPSCLOCK.tick(1) # 1秒間に1回ループが実行
